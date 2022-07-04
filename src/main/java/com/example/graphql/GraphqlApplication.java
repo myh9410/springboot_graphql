ffa1248d7188cda4b7d7e674af9a238b4732557f
moon <myh@gabia.com>
2022-07-04 19:10:27 +0900

    spring-boot-starter-graphql 사용, find, save 요청 생성

--- src/main/java/com/example/graphql/GraphqlApplication.java
+++ src/main/java/com/example/graphql/GraphqlApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphQLApplication {
+public class GraphqlApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(GraphQLApplication.class, args);
+        SpringApplication.run(GraphqlApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/example/graphql/dto/enums/YorN.java
@@ -0,0 +1,5 @@
+package com.example.graphql.dto.enums;
+
+public enum YorN {
+    Y, N
+}
