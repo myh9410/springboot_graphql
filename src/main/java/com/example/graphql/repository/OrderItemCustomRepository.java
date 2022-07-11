5169139778de4e09968135983b7bd1ed9cf56e05
myh9410 <mjskks94@naver.com>
2022-07-11 15:54:10 +0900

    orderItem entity에 item entity 활성화

--- src/main/java/com/example/graphql/repository/OrderItemCustomRepository.java
+++ src/main/java/com/example/graphql/repository/OrderItemCustomRepository.java
@@ -10,6 +10,7 @@ public class OrderItemCustomRepository {
     private final JPAQueryFactory jpaQueryFactory;
     private final QOrderItem orderItems = QOrderItem.orderItem;
     private final QOrder order = QOrder.order;
+    private final QItem item = QItem.item;
 
     public OrderItemCustomRepository(JPAQueryFactory jpaQueryFactory) {
         this.jpaQueryFactory = jpaQueryFactory;
@@ -18,6 +19,8 @@ public class OrderItemCustomRepository {
     public OrderItem getOrderItemsByNo(Long no) {
         return jpaQueryFactory.selectFrom(orderItems)
                 .join(orderItems.order, order)
+                .fetchJoin()
+                .join(orderItems.item, item)
                 .fetchJoin()
                 .where(orderItems.no.eq(no))
                 .fetchOne();
